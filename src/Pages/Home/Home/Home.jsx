cf862308638b26aaef1e80cdba7791ddd0f96bb9
Md Alve <mdalve1091@gmail.com>
2023-05-10 21:37:21 +0600

    About us section implement

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,3 +1,4 @@
+import About from "../About/About";
 import Banner from "../Banner/Banner";
 
 
@@ -5,7 +6,7 @@ const Home = () => {
     return (
         <div>
             <Banner></Banner>
-            <h2>This is home</h2>
+            <About></About>
         </div>
     );
 };
